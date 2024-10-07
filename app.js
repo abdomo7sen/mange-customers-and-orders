5be0cd3f2c5e98acc78663e8790d46377e480a35
abdomo7sen <abdomohsen20006@gmail.com>
2024-10-07 20:54:41 +0300

    update

--- app.js
+++ app.js
@@ -2,12 +2,15 @@ import express from 'express'
 import customerRouter from './modules/customer/customer.router.js'
 import productRouter from './modules/product/product.router.js'
 import orderRouter from './modules/orders/orders.router.js'
+
 const app = express()
 const port = 3000
-
+app.use(express.urlencoded({extended: true}))
 app.use(express.json())
+
 app.use('/auth',customerRouter)
 app.use('/products',productRouter)
 app.use('/orders',orderRouter)
+
 app.get('/', (req, res) => res.send('Hello World!'))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
--- database/dbConnection.js
+++ database/dbConnection.js
@@ -1,15 +1,16 @@
 import mysql from "mysql2"
-
+import {configDotenv} from 'dotenv'
+configDotenv({path:"./config/.env"})
 export const dbconnection = ()=>{
     const connection = mysql.createConnection({
-        host: "localhost",
-        user: "root",
-        password: "",
-        database: "mange_orders"
+        host:process.env.HOST,  
+        user: process.env.USER, 
+        password: process.env.PASSWORD,
+        database: process.env.DATABASE
     })
     connection.connect((err)=>{
         if(err){
-            console.log(err)
+            console.log(err.message)
         }
         else{
             console.log("Database connected")
